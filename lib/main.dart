03b70a8ded7bb754f5f5707dc318da2ac848f10d
ismail-24 <ismailtarek888@gmail.com>
2023-10-23 19:22:01 +0300

    debugShow

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class petoMarket extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
